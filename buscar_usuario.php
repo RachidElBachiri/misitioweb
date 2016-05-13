dc25366617bb057fb0591e11bab03a37a2fba6a0
RachidElBachiri <rachidelbachiri05@gmail.com>
2016-05-13 09:55:36 +0200

    ARCHIVO PARA LA BUSQUEDA DE USUARIOS

---
+++ buscar_usuario.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="Recursillos/bootstrap/css/bootstrap.css">
+    <link type="text/css" rel="stylesheet" href="Recursillos/Cas/jquery.fullpage.css">
+    <link type="text/css" rel="stylesheet" href="Recursillos/Css/nav.css">
+    <link rel="stylesheet" href="./busqueda_sancion.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+      <div class="container">
+       <div class="row">
+           <h2>Buscador</h2>
+           <div class="search">
+             <input type="text" class="form-control input-sm" maxlength="64" placeholder="Search" required="Campo Obligatorio"/>
+             <button type="submit" class="btn btn-primary btn-sm">Buscar</button>
+           </div>
+       </div>
+     </div>
+     <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+     <!-- usar siempre la última versión de jquery de los servidores de google. Yo recomiendo esta -->
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+     <script type="text/javascript" src="Recursillos/bootstrap/js/bootstrap.js"></script>
+  </body>
+</html>
