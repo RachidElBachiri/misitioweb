262c08d469afab9e9ee9a4e1172c5fefbf4c6029
RachidElBachiri <rachidelbachiri05@gmail.com>
2016-05-16 13:20:07 +0200

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php 
+	$host = "localhost";
+	$user = "root";
+	$pass = "alumno";
+	$db = "aridades1";
+	
+	$connect = new mysqli($host,$user,$pass,$db) or die ("error" . mysqli_errno($connect));
+	
+
+
+
+
+
+?>
