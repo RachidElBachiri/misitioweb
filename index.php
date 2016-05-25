1bee05a0216ecfd5ba27eb1f11894d98717abcc9
RachidElBachiri <rachidelbachiri05@gmail.com>
2016-05-25 15:51:16 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,236 @@
+
+<!DOCTYPE html>
+<html lnag="es">
+	<head>
+		<title>INDEX-YAMAHA</title>
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	    	<link type="text/css" rel="stylesheet" href="Recursillos/bootstrap/css/bootstrap.css">
+    		<link type="text/css" rel="stylesheet" href="Recursillos/Cas/jquery.fullpage.css">
+    		<link type="text/css" rel="stylesheet" href="Recursillos/Css/nav.css">
+    		<link rel="stylesheet" href="ccs3/busqueda.css" media="screen" title="no title" charset="utf-8">
+
+
+		<link href="css3/yamaha.css">
+		<style type="text/css">
+			
+			* {
+				margin:0px;
+				padding:0px;
+			}
+			
+			#header {
+				margin:auto;
+				width:500px;
+				font-family:Arial, Helvetica, sans-serif;
+			}
+			
+			ul, ol {
+				list-style:none;
+			}
+			
+			.nav > li {
+				float:left;
+			}
+			
+			.nav li a {
+				background-color:#000;
+				color:#fff;
+				text-decoration:none;
+				padding:10px 12px;
+				display:block;
+			}
+			
+			.nav li a:hover {
+				background-color:#434343;
+			}
+			
+			.nav li ul {
+				display:none;
+				position:absolute;
+				min-width:140px;
+			}
+			
+			.nav li:hover > ul {
+				display:block;
+			}
+			
+			.nav li ul li {
+				position:relative;
+			}
+			
+			.nav li ul li ul {
+				right:-140px;
+				top:0px;
+			}
+			
+		</style>
+		<nav>
+		<!-- Buscador -->
+
+
+
+
+	
+		
+
+	
+
+		
+		<center><ol class="breadcrumb">
+  			<li class="active"><img src="imagenes/descarga.png"</li>
+		</ol>
+	
+	<br><hr><br>
+		<div id="header">
+			<ul class="nav">
+				<li><a href="buscador-Usuarios/index.php">Buscar</a></li>
+				<li><a href="php/login.php">Iniciar Sesión</a></li>
+				<li><a href="html/registro.html">Registrate</a></li>
+				<li><a href="">Yamaha</a></li>
+				<li><a href="html/accesorios.html">Accesorios-Motor</a>
+					<ul>
+						<li><a href="html/insertarcilindro.php">Cilindros</a></li>
+						<li><a href="">Cigueñal & Pistoacuten</a></li>
+						<li><a href="">V&aacutelvula</a></li>
+						<li><a href="">Arbol de Levas & Cadena</a></li>
+						<li><a href="">Bomba de Aceite</a></li>
+						<li><a href="">Admisi&oacuten</a></li>
+						<li><a href="">MasSubmenus</a>
+							<ul>
+								<li><a href="">Submenu1</a></li>
+								<li><a href="">Submenu2</a></li>
+								<li><a href="">Submenu3</a></li>
+								<li><a href="">Submenu4</a></li>
+							</ul>
+						</li>
+					</ul>
+				</li>
+				<li><a href="">Accesorios-Chasis</a>
+					<ul>
+						<li><a href="">Submenu1</a></li>
+						<li><a href="">Submenu2</a></li>
+						<li><a href="">Submenu3</a></li>
+						<li><a href="">Submenu4</a></li>
+					</ul>
+				</li>
+				<center><li><a href="">Modelos</a>
+					<ul>
+						<li><a href="">MT-09 Sport Tracker</a></li>
+						<li><a href="">MT-09 Street Rally</a></li>
+						<li><a href="">MT-07 Moto Cage</a></li>
+					</ul>
+
+				</li>
+					<li><a href="">Tipos</a>
+						<ul>
+							<li><a href="">MotoCross</a></li>
+							<li><a href="">enduro</a></li>
+							<li><a href="">Acuaticas</a></li>
+						</ul>
+					</li>							
+			</ul>
+		</div>
+		<div>
+	</nav>
+</head>	
+		<center><body data-spy="scroll" data-target=".navbar">	
+
+	<br><br><br><hr>
+	
+			<div class="row"> <!-- Creamos el contenedor para el slider -->
+				<div class="ism-slider" data-transition_type="zoom" data-play_type="loop" data-interval="3000" id="my-slider">
+			  <ol>
+			    <li>
+					<p>Textod e la 1 </p>
+			      <img src="ism/image/slides/_u/1.jpg"><!-- Introducimos la imagen 1 -->
+					
+			    </li>
+					
+			    <li>
+			      <img src="ism/image/slides/_u/2.jpg"><!-- Introducimos la imagen 2 -->
+			    </li>
+			    <li>
+			      <img src="ism/image/slides/_u/3.jpg"><!-- Introducimos la imagen 3 -->
+			    </li>
+			    <li>
+			      <img src="ism/image/slides/_u/4.jpg"><!-- Introducimos la imagen 4 -->
+			    </li>
+			    <li>
+			      <img src="ism/image/slides/_u/5.jpg"><!-- Introducimos la imagen 5 -->
+			    </li>
+			    <li>
+			      <img src="ism/image/slides/_u/6.jpg"><!-- Introducimos la imagen 6 -->
+			    </li>
+			    <li>
+			      <img src="ism/image/slides/_u/7.jpg"><!-- Introducimos la imagen 7 -->
+			    </li>
+			    <li>
+			      <img src="ism/image/slides/_u/8.jpg"><!-- Introducimos la imagen 8 -->
+			    </li>
+			    <li>
+			      <img src="ism/image/slides/_u/9.jpg"><!-- Introducimos la imagen 9 -->
+			    </li>
+			    <li>
+			      <img src="ism/image/slides/_u/10.jpg"><!-- Introducimos la imagen 10 -->
+			    </li>
+			  </ol>
+			</div>
+
+	  
+	  
+	  
+	  
+	  <br><br>
+	  <!-- fin Codigo del carrusels -->
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <center><h3>El Bachiri Bachiri Rachid 2016 Yamaha</h3>
+        <p style="font-size:20px; font-color:#777">Realizado en Bootstrap</p>
+        <p>
+          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">YAMAHA</a>
+        </p>
+      </div>
+
+    </div> <!-- /container -->
+
+    <footer>
+      <div class="container well">
+  	     <h2><center>Yamaha</center></h2>
+      </div>
+      <div id="footer">
+          <div class="container">
+              <div class="row">
+                  <h3 class="footertext"></h3>
+                  <br>
+                    <div class="col-md-4">
+                      <center>
+                        <img src="imagenes/azul-yamaha.jpg" class="img-circle" alt="the-brains">
+                        <br>
+                        
+                  <p><center><a href="#">Realizado con Bootstrap</a> <p class="footertext">Copyright 2016</p></center></p>
+              </div>
+          </div>
+      </div>
+	  
+	  <!-- AQUI ABAJO ABRIMOS UNAS ETIQUETAS DEL TIPO SCRIPT PARA AGREGAR ARCHIVOS EXTERNOS DEL FAMOSO LENGUAJE DE PROGRAMCIÓN DE CARA AL CLIENTE O LOCAL.
+						PARA NUESTRO CASO SERÁN TRES LOS ARCHIVOS ..-->
+	  
+    </footer>
+
+    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <!-- usar siempre la última versión de jquery de los servidores de google. Yo recomiendo esta -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script type="text/javascript" src="Recursillos/bootstrap/js/bootstrap.js"></script>
+	
+	    <script src="html/recursillos/js/jquery.js"></script>
+    <script src="html/recursillos/js/bootstrap.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $('.myCarousel').carousel({
+			interval:3000
+		});
+        });
+    </script>
+
+	</body>
+</html>
